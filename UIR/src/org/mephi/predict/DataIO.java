f671053c3f84703251eba2562dc4f8d081d88c1a
TresRus <tresrus92@gmail.com>
2013-05-21 13:04:39 +0400

    Add new data class

---
+++ UIR/src/org/mephi/predict/DataIO.java
@@ -0,0 +1,13 @@
+package org.mephi.predict;
+
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+public class DataIO {
+	List<double[]> dataIn;
+	List<double[]> dataOut;
+	
+	
+}
